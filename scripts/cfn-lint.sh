f5d68305236c6eeb4a30bef89e84bfb2e25d3473
Leng Sophanith <leng.sophanith@mbanq.com>
2021-08-10 10:36:20 +0700

    update cfn-lint.sh from reach

--- scripts/cfn-lint.sh
+++ scripts/cfn-lint.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
+#shellcheck disable=SC2012
 echo "Cfn linter checks..."
-cfn_list=$(ls ./*.yml)
-
-for cfn in $cfn_list; do
-    cfn-lint "$cfn" --ignore-checks W >reports/"${cfn}"_lint.out.json && echo OK || echo FAILED
-    cfn_lint_output=$(cat reports/"${cfn}"_lint.out.json)
-
+​
+path=$1
+isarray=$2
+​
+if [[ $isarray == 1 ]]; then 
+    cfn_list=$(ls "$path")
+​
+    for cfn_report_path in $cfn_list; do
+        echo "cfn path is ${cfn_report_path}"
+        cfn_file=$(ls "$cfn_report_path" | tr '\n' '\0' | xargs -0 -n 1 basename)
+        echo cfn is "${cfn_file}"
+        cfn-lint "$cfn_report_path" --ignore-checks W > reports/"${cfn_file}"_lint.out.json && echo OK || echo FAILED
+        cfn_lint_output=$(cat reports/"${cfn_file}"_lint.out.json)
+​
+        if [ "$cfn_lint_output" != "" ]; then
+            echo please check in "reports/${cfn_file}"_lint.out.json
+            exit
+        fi
+    done
+else
+    cfn-lint "$path" --ignore-checks W >reports/cfn_lint.out.json && echo OK || echo FAILED
+    cfn_lint_output=$(cat reports/cfn_lint.out.json)
+​
     if [ "$cfn_lint_output" != "" ]; then
-        echo please check in "${cfn}"_lint.out.json
+        echo please check in reports/cfn_lint.out.json
         exit
     fi
-done
+fi
