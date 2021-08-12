04588b001fd9d7d08d7c371d70c28ca2904ad0e0
sophanith2020 <68974451+sophanith2020@users.noreply.github.com>
2021-08-12 20:25:48 +0700

    Update metric.sh

--- scripts/metric.sh
+++ scripts/metric.sh
@@ -10,7 +10,6 @@
 #
 #    bash scripts/pipeline/metrics.sh
 set -eo pipefail
-echo SONAR_TOKEN is "${SONAR_TOKEN}"
 
 echo "Verify code metrics..."
-sonar-scanner -Dsonar.host.url=https://sonar.dev.mbanq.cloud -Dsonar.login=688680b3376217a389039b5c68be0e8ab1ac9d9e
+sonar-scanner -Dsonar.host.url=https://sonar.dev.mbanq.cloud -Dsonar.login=${{ secrets.SONAR_TOKEN }}
